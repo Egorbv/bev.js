5ddc01f2fd7c4ce41f74ec06eee03e451e264815
EBelov <EBelov@avanpost.ru>
2014-06-16 12:22:04 +0400

    add popup control

---
+++ kernel.js
@@ -0,0 +1,30 @@
+﻿function Bev()
+{
+	this.IsRTL = true;
+}
+
+Bev.prototype.AttachEvent = function(domElement, ev, handler)
+{
+	if (domElement.attachEvent)
+	{
+		domElement.attachEvent("on" + ev, handler);
+	}
+	else
+	{
+		domElement.addEventListener(ev, handler);
+	}
+}
+
+Bev.prototype.DetachEvent = function (domElement, ev, handler)
+{
+	if (domElement.detachEvent)
+	{
+		domElement.detachEvent("on" + ev, handler);
+	}
+	else
+	{
+		domElement.removeEventListener(ev, handler);
+	}
+}
+
+var bev = new Bev();
---
+++ popup.js
@@ -0,0 +1,48 @@
+﻿function Popup(setting)
+{
+	var e = event;
+	var x = e.clientX + document.body.scrollLeft;
+	var y = e.clientY + document.body.scrollTop;
+
+
+	this.Panel = document.createElement("div");
+	this.Panel.className = "popup";
+	this.Panel.style.width = "200px";
+	this.Panel.style.height = "200px";
+	this.Panel.style.top = y + "px";
+	this.Panel.style.left = x + "px";
+
+
+
+	if (setting != null)
+	{
+		if (setting.element != null)
+		{
+			alert(5);
+		}
+		if (setting.conent != null)
+		{
+
+		}
+	}
+
+	document.body.appendChild(this.Panel);
+
+	var instance = this;
+	this.Panel.onmousedown = function () { instance.PanelClick() };
+	this._bodyClick = function () { instance.BodyClick(); };
+	bev.AttachEvent(document, "mousedown", this._bodyClick);
+}
+
+//Обработка клика вне нажатия всплывающей панели
+Popup.prototype.BodyClick = function ()
+{
+	bev.DettachEvent(document, "mousedown", this._bodyClick);
+	document.body.removeChild(this.Panel);
+}
+
+//Обработчик клика в сплывающей панели
+Popup.prototype.PanelClick = function ()
+{
+	alert(5);
+}
